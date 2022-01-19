9339b604905648e0988297f2a9c97a674e89207b
Shyren <shyren.more30@gmail.com>
2022-01-19 14:09:21 +0530

    Auth done, initialise all pages

---
+++ frontend/src/Hooks/react-query/dashboard-hooks.js
@@ -0,0 +1,79 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const SERVER_BASE_URL = process.env.REACT_APP_SERVER_BASE_URL;
+
+export const useGetCategoryCountQuery = () => {
+    return useQuery(["category-count"], async () => {
+        try {
+            const res = await axios.get(`${SERVER_BASE_URL}/get-category-count/`, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+            });
+            // console.log("axios resp: ", res);
+            return res.data;
+        } catch (err) {
+            console.log("axios err: ", err);
+            throw new Error(err.response.data.message);
+        }
+    });
+};
+
+export const useGetRecentTransactionsQuery = () => {
+    return useQuery(["recent-transactions"], async () => {
+        try {
+            const res = await axios.get(`${SERVER_BASE_URL}/get-n-expenditures/5/`, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+            });
+            console.log("axios resp: ", res);
+            return res.data;
+        } catch (err) {
+            console.log("axios err: ", err);
+            throw new Error(err.response.data.message);
+        }
+    });
+};
+
+export const useHeatmapInputQuery = () => {
+    return useQuery(["heatmap"], async () => {
+        try {
+            const res = await axios.get(`${SERVER_BASE_URL}/expenditure-heatmap/`, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+            });
+            // console.log("axios resp: ", res);
+            return res.data;
+        } catch (err) {
+            console.log("axios err: ", err);
+            throw new Error(err.response.data.message);
+        }
+    });
+};
+
+export const useGetAllExpenseQuery = () => {
+    return useQuery(["all-expense"], async () => {
+        try {
+            const res = await axios.get(`${SERVER_BASE_URL}/allexpenditures/`, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+            });
+            // console.log("axios resp: ", res);
+            return res.data;
+        } catch (err) {
+            console.log("axios err: ", err);
+            throw new Error(err.response.data.message);
+        }
+    });
+};
+export const useGetCategoryByMonthQuery = () => {
+    return useQuery(["get-category-by-month"], async () => {
+        try {
+            const res = await axios.get(`${SERVER_BASE_URL}/get-category-by-month/`, {
+                headers: { Authorization: `Bearer ${localStorage.getItem("authToken")}` },
+            });
+            console.log("axios resp: ", res);
+            return res.data;
+        } catch (err) {
+            console.log("axios err: ", err);
+            throw new Error(err.response.data.message);
+        }
+    });
+};
+
---
+++ frontend/src/pages/dashboard/Dashboard.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import DoughnutChart from '../../components/Charts/DoughnutChart';
+import './Dashboard.css'
+import { Chart, ArcElement } from 'chart.js'
+import Heatmap from '../../components/Charts/Heatmap';
+import RecentTransactions from '../../components/Charts/RecentTransactions';
+import PercentChangeInExpenditure from '../../components/Charts/PercentChangeInExpenditure';
+import StackedBarChart from '../../components/Charts/StackedBarChart';
+Chart.register(ArcElement);
+
+// pass two arrays as props to legend component
+
+
+const Dashboard = () => {
+    
+    return (
+        <>
+            <div className="justify-content-center align-items-center">
+                <div className="row">
+                    <div className="col-md-4 col-sm-12 col-xs-12 col-12 mx-2 my-2">
+                        <DoughnutChart />
+                    </div>
+                    <div className="col-md-4 col-sm-12 col-xs-12 col-12 mx-2 my-2">
+                        <Heatmap />
+                    </div> 
+                </div>
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-md-6 col-sm-12 col-xs-12 col-12 mx-2 my-2">
+                        <RecentTransactions />
+                    </div>
+                    
+                    <div className="col-md-4 col-sm-12 col-xs-12 col-12 mx-2 my-2 ">
+                        <PercentChangeInExpenditure />
+                    </div>
+                    
+                </div>
+                
+                <div className="row d-flex justify-content-center align-items-center">
+                    <div className="col-md-6 col-sm-12 col-xs-12 col-12 mx-2 my-2">
+                        <StackedBarChart />
+                    </div>
+                    
+                    
+                    
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Dashboard;
