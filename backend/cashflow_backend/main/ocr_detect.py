afeda7d0e2fc19798488328095801512fd4ac0c3
Varun Mamtora <varunmamtora@gmail.com>
2022-01-20 01:02:52 +0530

    add ocr

---
+++ backend/cashflow_backend/main/ocr_detect.py
@@ -0,0 +1,12 @@
+from .cashflow_complete import *
+import cv2
+
+def extract_data(img_path):
+    img = cv2.imread(f"../cashflow_backend/{img_path}")
+
+    d = predictIMG(img)
+
+    print("data")
+    print(d)
+
+    return d
